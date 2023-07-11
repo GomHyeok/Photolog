83953bc2000cbeb47a32366741c4b25f8b5e43c9
gomhyeok <gomhyeok@gmail.com>
2023-07-11 22:56:30 +0900

    Initial Commit

---
+++ pureLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pureLocation
+//
+//  Created by 최재혁 on 2023/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
